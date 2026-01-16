e3510e069850cc750a0a1cc48628b9e9b4a4c0d5
Faheema Mariyam <faheemamariyam2102004@gmail.com>
2026-01-16 16:27:41 +0530

    fetch properties for rag

--- ai_gateway/urls.py
+++ ai_gateway/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import AreaInsightsGateway
+from .views import AreaInsightsGateway,PropertiesForRAG
 
 urlpatterns = [
     path("area-insights/", AreaInsightsGateway.as_view()),
+    path("properties/", PropertiesForRAG.as_view()),
 ]
